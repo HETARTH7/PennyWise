47b577a4d8d6d24cbf62b233b12e3ed5c4a3393d
Hetarth <hetarth2028@gmail.com>
2024-12-05 16:43:35 +0530

    created angular components and completed landing page compoenent

---
+++ client/src/app/landing/landing.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-landing',
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  templateUrl: './landing.component.html',
+  styleUrl: './landing.component.css',
+})
+export class LandingComponent {}
